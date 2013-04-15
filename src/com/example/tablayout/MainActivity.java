2f11588a530ca9f9c437f32ae2e24e651b4c93df
asuracry <jrijsdijk612@gmail.com>
2013-04-15 17:39:24 +0200

    Opzet project met werkende tabs

---
+++ src/com/example/tablayout/MainActivity.java
@@ -0,0 +1,26 @@
+/*
+ * Author : ErVaLt / techwavedev.com
+ * Description : TabLayout Andorid App
+ */
+package com.example.tablayout;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class MainActivity extends Activity implements OnClickListener {
+
+	/** Called when the activity is first created. */
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.tab1);
+
+	}
+
+	public void onClick(View src) {
+
+	}
+}
---
+++ src/com/example/tablayout/SetupActivity.java
@@ -0,0 +1,17 @@
+/*
+ * Author : ErVaLt / techwavedev.com
+ * Description : TabLayout Andorid App
+ */
+package com.example.tablayout;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SetupActivity extends Activity {
+
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.tab2);
+	}
+
+}
