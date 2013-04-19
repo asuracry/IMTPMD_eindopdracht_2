597910e798477fe9a2632922f96525f0df002f68
asuracry <jrijsdijk612@gmail.com>
2013-04-19 14:28:01 +0200

    Server client communicatie werkt op tab en op computer, externe communicatie werkt ook als firewall uitstaat. Ipadress bug gefixed

--- src/com/example/tablayout/MainActivity.java
+++ src/com/example/tablayout/MainActivity.java
@@ -8,6 +8,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends Activity implements OnClickListener {
@@ -24,14 +25,38 @@ public class MainActivity extends Activity implements OnClickListener {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.tab1);
+		Button b = (Button)findViewById(R.id.button1);
+		b.setOnClickListener(this);
+		System.out.println("hij pakt de onclick");
 
 	}
 
 	public void onClick(View src) {
+		EditText naamEditText = (EditText) this.findViewById(R.id.naamVeld); 
+		naam = naamEditText.getText().toString();
+		
+		EditText poortNummerEditText = (EditText) this.findViewById(R.id.poortNummerVeld); 
+		poortNummer = Integer.parseInt(poortNummerEditText.getText().toString());
+		
+		EditText opdrachtEditText = (EditText) this.findViewById(R.id.opdrachtVeld); 
+		opdracht = opdrachtEditText.getText().toString();
+		
+		
+		EditText ipAdressEditText = (EditText) this.findViewById(R.id.ipAdressVeld); 
+		ipAdress = ipAdressEditText.getText().toString();
+		
+		String bericht = naam + " zegt: " + opdracht;
+		
+		System.out.println("ipadress:" + ipAdress);
+		System.out.println("poortNummer:" + poortNummer);
+		System.out.println("bericht:" + bericht);
+		
+		serverCommunicator = new ServerCommunicator(this, ipAdress, poortNummer, bericht);
+		System.out.println("hij verzend");
 
 	}
 	
-	public void VerstuurBericht(View v)
+	/*public void VerstuurBericht(View v)
 	{
 		EditText naamEditText = (EditText) this.findViewById(R.id.naamVeld); 
 		naam = naamEditText.getText().toString();
@@ -51,4 +76,7 @@ public class MainActivity extends Activity implements OnClickListener {
 		serverCommunicator = new ServerCommunicator(this, ipAdress, poortNummer, bericht);
 		
 	}
+	/*
+	 * 
+	 */
 }
